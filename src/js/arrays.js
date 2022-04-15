9192caf17f4760902e64b9f61e4451245c22e68e
Anzhelika <lientobe@gmail.com>
2022-04-15 03:30:16 +0300

    fixed every

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -81,7 +81,7 @@ function some(array, callback) {
 */
 function every(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    if(callback (array[i], i, array)) {
+    if(!(callback (array[i], i, array))) {
       return false;
     }
   }
